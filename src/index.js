08535d3acea68232273f0821c6494205a80bb8c8
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-03-01 14:01:31 +0100

    Add HTML dynamically

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './style.css';
+import list from './leaderboardList.js';
+
+const listContainer = document.querySelector('.board-list');
+const refreshButton = document.querySelector('.board-btn');
+const refreshList = () => {
+  listContainer.innerHTML = '';
+  list.forEach((item) => {
+    listContainer.innerHTML += `<li class="scores">${item.name}: ${item.score}</li>`;
+  });
+  return 0;
+};
+
+refreshList();
+
+refreshButton.addEventListener('click', () => {
+  refreshList();
+});
---
+++ src/leaderboardList.js
@@ -0,0 +1,28 @@
+const list = [
+  {
+    name: 'ismail',
+    score: 50,
+  },
+  {
+    name: 'Name',
+    score: 30,
+  },
+  {
+    name: 'Name',
+    score: 40,
+  },
+  {
+    name: 'Name',
+    score: 10,
+  },
+  {
+    name: 'Name',
+    score: 30,
+  },
+  {
+    name: 'Name',
+    score: 25,
+  },
+];
+
+export default list;
